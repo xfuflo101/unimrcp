211ec4bd17fe0deb193cba4bf9897593bd07c432
xfuflo <xfuflo@rambler.ru>
2023-02-20 05:13:21 +0300

    encapsulating mrcp messages codec

--- libs/mrcp-mediatel/src/apt_mediatel.cpp
+++ libs/mrcp-mediatel/src/apt_mediatel.cpp
@@ -1,39 +1,86 @@
 #include <thread>
 #include <mutex>
 
-#include "apt_pool.h"
+
+#include <apr.h>
+
+
+// mrcp_resource_factory_t *factory;
+// mrcp_resource_loader_t *resource_loader;
+// resource_loader = mrcp_resource_loader_create(TRUE,suite->pool);
+// if(!resource_loader) {
+//   apt_log(APT_LOG_MARK,APT_PRIO_WARNING,"Failed to Create Resource Loader");
+//   return FALSE;
+// }
+//
+// factory = mrcp_resource_factory_get(resource_loader);
+// if(!factory) {
+//   apt_log(APT_LOG_MARK,APT_PRIO_WARNING,"Failed to Create Resource Factory");
+//   return FALSE;
+// }
+
 
 
 typedef struct __mrcp_common {
 
-    mrcp_common():
-      _pool(apt_pool_create())
-    {}
-
-    ~mrcp_common() {
-        apr_pool_destroy(_pool);
-        _pool = nullptr;
-    }
-
     apr_pool_t * _pool;
+    mrcp_resource_loader_t * _resource_loader;
+    mrcp_resource_factory_t * _factory;
 
 } mrcp_common;
 
 
 static std::mutex g_mrcp_mutex;
-static mrcp_common * g_mrcp_common = nullptr;
+static std::unique_ptr<mrcp_common> g_mrcp_common;
 
 
 namespace mrcp {
 
 
-void initialize() {
+bool initialize() {
 
     std::lock_guard<std::mutex> lock(g_mrcp_mutex);
 
     if (!g_mrcp_common) {
-		    g_mrcp_common = new mrcp_common();
+
+        /* APR global initialization */
+        if(apr_initialize() != APR_SUCCESS) {
+            apr_terminate();
+            return false;
+        }
+
+		    std::unique_ptr<mrcp_common> tmp = std::make_unique<mrcp_common>();
+
+        /* create APR pool */
+        tmp->_pool = apt_pool_create();
+        if(!tmp->_pool) {
+            apr_terminate();
+            return false;
+        }
+
+        /** Load resource factory */
+        tmp->resource_loader = mrcp_resource_loader_create(TRUE, tmp->_pool);
+        if(!tmp->resource_loader) {
+            /* destroy APR pool */
+            apr_pool_destroy(pool);
+            /* APR global termination */
+            apr_terminate();
+            return false;
+        }
+
+        tmp->factory = mrcp_resource_factory_get(tmp->resource_loader);
+      	if(!tmp->factory) {
+            /* destroy APR pool */
+            apr_pool_destroy(pool);
+            /* APR global termination */
+            apr_terminate();
+            return false;
+      	}
+
+		    g_mrcp_common = tmp;
     }
+
+    return true;
 }
 
 
@@ -41,9 +88,15 @@ void terminate() {
 
     std::lock_guard<std::mutex> lock(g_mrcp_mutex);
 
-    if (g_mrcp_common) {
-        delete g_mrcp_common;
-        g_mrcp_common = nullptr;
+    std::unique_ptr<mrcp_common> tmp(g_mrcp_common.release());
+
+    if (tmp) {
+
+        mrcp_resource_factory_destroy(tmp->factory);
+        /* destroy APR pool */
+        apr_pool_destroy(pool);
+        /* APR global termination */
+        apr_terminate();
     }
 }
 
